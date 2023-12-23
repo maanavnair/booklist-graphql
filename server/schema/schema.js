13395e242166c6a5c7f487500d721af80ca0b73c
Maanav Nair <maanavnair20042@gmail.com>
2023-12-23 18:16:11 +0530

    updated resolve function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,13 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString } = graphql;
+const _ = require('lodash');
+
+//dummy data
+var books = [
+    {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'},
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -18,6 +26,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLString } },
             resolve(parent, args){
                 //code to get data from db/other source
+                return _.find(books, { id: args.id });
             }
         }
     }
